37dd14fe5718abe7fc9cc3abd4d5d15f6fe8b722
Manibharathi <manibharath159@gmail.com>
2024-03-19 18:51:06 +0530

    Operators - Arithmetic,Assignment

---
+++ JAVASCRIPT/Operators/arithmetic.js
@@ -0,0 +1,74 @@
+// var a = 10;
+// var b = 20;
+// var c = a+b; // Addition
+// console.log(c);
+ 
+// c = a-b; //Subtraction
+
+// console.log(c);
+
+// c = a*b; // Multiplication
+// console.log(c);
+
+// c = a/b; // Division
+// console.log(c);
+
+// var d = 10;
+// var e = 60;
+
+// c = d%e; //Modulus Operator
+// console.log(c);
+
+/**
+ * Task 1 Swapping between two numbers
+ */
+
+// var f = 10;
+// var g = 20;
+// var h;
+
+// console.log("Before Swapping");
+// console.log(f);
+// console.log(g);
+
+// h = f; // 10
+// f = g; // 20
+// g = h; // 10
+
+// console.log("After Swapping");
+// console.log(f);
+// console.log(g);
+
+var a = 10;
+var b = 20;
+console.log("Before Swapping");
+console.log(a);
+console.log(b);
+// a = a+b;
+// b = a-b;
+// a = a-b;
+//a = a+b-(b = a);
+a = (a*b)/(b=a);
+console.log("After Swapping");
+console.log(a);
+console.log(b);
+
+//Assignment Operators
+
+a = 10;
+a += 3;
+console.log(a);
+a = 15;
+b = 2;
+b += a;
+console.log(b);
+
+//a *=2;
+console.log(a);
+
+//a = 10**2;
+a = 10;
+a **=2; 
+console.log(a);
+
+
---
+++ JAVASCRIPT/example.js
@@ -0,0 +1,15 @@
+var myName = "kumar"; 
+console.log(myName);
+
+var myAge = 20; //number
+myAge = "Sankar";
+var address = "Coimbatore"; //String 
+
+var mobileNumber = 9944033729 //Long Numbers
+
+var degree = 98.7 // Float or Double
+
+var isValid = true; // Boolean
+
+
+
