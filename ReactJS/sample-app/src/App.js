98f9f30177f9dbe3ccd0afbe7ce029dc19f1837a
Manibharathi <manibharath159@gmail.com>
2024-05-21 19:48:35 +0530

    State is updated

--- ReactJS/sample-app/src/App.js
+++ ReactJS/sample-app/src/App.js
@@ -3,13 +3,45 @@ import './App.css';
 import Header from './header';
 import Content from './content';
 import Footer from './footer';
+import { useState } from 'react';
 
 function App(){
+
+  //let count = 0;
+  //useState -> hooks
+  // let[variableName,setMethod] = useState(initialState)
+  let [count,setCount] = useState(0);
+
+  let [data,setData] = useState("Kavi")
+
+  function changeCount()
+  {
+    let value = count+1;
+    setCount(value);
+    console.log("Count",count);
+    let name = "Harshan-"+count;
+    setData(name);
+  }
+
   return(
     <>
-      <Content kavi="My Content" area="Coimbatore"/>
+      {/* <Content kavi="My Content" area="Coimbatore"/>
       <Header kavi="My Headers" native="Nagercoil"/>
-      <Footer kavi="My Footers" location="Chennai"/>
+      <Footer kavi="My Footers" location="Chennai"/> */}
+
+      {
+        count
+      }
+
+      <button onClick={()=>changeCount()}>
+        Change Count
+      </button>
+
+      {
+        data
+      }
+
+
       
     </>
 );
