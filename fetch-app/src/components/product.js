e48381d6840d2288a8c4ad85ced25c02509ef5dc
Manibharathi <manibharath159@gmail.com>
2024-07-19 19:40:06 +0530

    Get Products

--- fetch-app/src/components/product.js
+++ fetch-app/src/components/product.js
@@ -3,17 +3,32 @@ import {useState,useEffect} from 'react';
 const Product = () =>{
     const[product,setProduct] = useState([]);
 
+    const[item,setItem] = useState({});
+    const[clicked,isClicked] = useState(false);
+
+    const fetchProduct = (id) =>{
+        //alert("Product id "+id);
+        fetch("https://fakestoreapi.com/products/"+id,
+            {
+                "method":"get"
+            }
+        ).then(response=>response.json())
+        .then(data=>{
+            isClicked(true);
+            setItem(data);
+            //console.log("item length "+item.length);
+        })
+    }
+
+    const goBack = ()=>{
+        isClicked(false);
+    }
+
+    // function fetchProduct(id){
+
+    // }
 
     const getProducts = () =>{
-
-        // fetch(URL,{
-        //     method:"GET"
-        // }).then(response=>response.json())
-        // .then(data=>{
-        //     setProduct(data);
-        // });
-
-
 
         fetch("https://fakestoreapi.com/products",
             {
@@ -30,7 +45,26 @@ const Product = () =>{
         getProducts()
       }, [])
 
+    if(clicked === true)
+        {
+            return( 
+            <div className="products">
+                <div key={item.id} className="myProducts">
+                    <img src={item.image} width="80px"/>
+                    <h1>Name of the Product</h1>
+                    <h3>{item.title}</h3>
+                    <h1>Description</h1>
+                    <p>{item.description}</p>
+                    <h1>Price - {item.price}</h1>
+                    {/* <h4>Rating {item.rating.rate}/5</h4> */}
+                </div> 
+                <button onClick={()=>goBack()}>Back To All Products</button>
+                </div>
+)
+        }  
 
+        else
+        {
     return(
         <>
             <h1>My Products</h1>
@@ -39,7 +73,7 @@ const Product = () =>{
                 {
                     product.map((item)=>{
                         return(
-                                        <div className="myProducts">
+                                        <div key={item.id} className="myProducts" onClick={()=>fetchProduct(item.id)}>
                                             <img src={item.image} width="80px"/>
                                             <h1>Name of the Product</h1>
                                             <h3>{item.title}</h3>
@@ -59,5 +93,6 @@ const Product = () =>{
             </div>
         </>
     );
+}
 }
 export default Product;
