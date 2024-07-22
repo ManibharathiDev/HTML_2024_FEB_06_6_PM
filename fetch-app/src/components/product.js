ae5297f6c1ed5b4dee10d461f3407c4a496f17ac
Manibharathi <manibharath159@gmail.com>
2024-07-22 18:52:51 +0530

    Fetch API Template Design

--- fetch-app/src/components/product.js
+++ fetch-app/src/components/product.js
@@ -41,6 +41,14 @@ const Product = () =>{
         });
     }
 
+    const titleTruncate = (text) =>{
+        return text.length > 15 ? text.substring(0,14) +"...":text;
+    }
+
+    const truncate = (text) =>{
+        return text.length > 80 ? text.substring(0,80) +"...":text;
+    }
+
     useEffect(() => {
         getProducts()
       }, [])
@@ -68,9 +76,128 @@ const Product = () =>{
     return(
         <>
             <h1>My Products</h1>
+            <div className='products'>
+                {
+                    product.map((item)=>{
+                        return(
+                            
+                                <div className='colss' key={item.id} onClick={()=>fetchProduct(item.id)}>
+                    
+                    <img src={item.image} width="80px" height="80px"/>
+                                            <p>{titleTruncate(item.title)}</p>
+                                            <p>{truncate(item.description)}</p>
+                                            <h1 className='productPrice'>Price - {item.price}</h1>
+                                            <h4>Rating {item.rating.rate}/5</h4>
+                                        
+                                </div>
+                            
+                        )
+                    })
+                }
+            </div>
+
+
             <div className="products">
 
-                {
+                {/* <div className='row'>
+                    <div className='cols'>
+                    
+                                            <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" width="80px"/>
+                                            <p>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</p>
+                                        
+                                            <p>Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</p>
+                                            <h1>Price - 1000</h1>
+                                            <h4>Rating 4.5/5</h4>
+                                         
+                    </div>
+
+                    <div className='cols'>
+                    <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" width="80px"/>
+                                            <p>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</p>
+                                        
+                                            <p>Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</p>
+                                            <h1>Price - 1000</h1>
+                                            <h4>Rating 4.5/5</h4>
+                    </div>
+
+                    <div className='cols'>
+                    <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" width="80px"/>
+                                            <p>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</p>
+                                        
+                                            <p>Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</p>
+                                            <h1>Price - 1000</h1>
+                                            <h4>Rating 4.5/5</h4>
+                    </div>
+
+                    <div className='cols'>
+                    <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" width="80px"/>
+                                            <p>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</p>
+                                        
+                                            <p>Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</p>
+                                            <h1>Price - 1000</h1>
+                                            <h4>Rating 4.5/5</h4>
+                    </div>
+
+                    <div className='cols'>
+                    <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" width="80px"/>
+                                            <p>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</p>
+                                        
+                                            <p>Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</p>
+                                            <h1>Price - 1000</h1>
+                                            <h4>Rating 4.5/5</h4>
+                    </div>
+                    </div>
+
+                    <div className='row'>
+                    <div className='cols'>
+                    
+                                            <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" width="80px"/>
+                                            <p>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</p>
+                                        
+                                            <p>Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</p>
+                                            <h1>Price - 1000</h1>
+                                            <h4>Rating 4.5/5</h4>
+                                         
+                    </div>
+
+                    <div className='cols'>
+                    <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" width="80px"/>
+                                            <p>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</p>
+                                        
+                                            <p>Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</p>
+                                            <h1>Price - 1000</h1>
+                                            <h4>Rating 4.5/5</h4>
+                    </div>
+
+                    <div className='cols'>
+                    <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" width="80px"/>
+                                            <p>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</p>
+                                        
+                                            <p>Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</p>
+                                            <h1>Price - 1000</h1>
+                                            <h4>Rating 4.5/5</h4>
+                    </div>
+
+                    <div className='cols'>
+                    <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" width="80px"/>
+                                            <p>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</p>
+                                        
+                                            <p>Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</p>
+                                            <h1>Price - 1000</h1>
+                                            <h4>Rating 4.5/5</h4>
+                    </div>
+
+                    <div className='cols'>
+                    <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" width="80px"/>
+                                            <p>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</p>
+                                        
+                                            <p>Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</p>
+                                            <h1>Price - 1000</h1>
+                                            <h4>Rating 4.5/5</h4>
+                    </div>
+                    </div> */}
+
+                {/* {
                     product.map((item)=>{
                         return(
                                         <div key={item.id} className="myProducts" onClick={()=>fetchProduct(item.id)}>
@@ -84,7 +211,7 @@ const Product = () =>{
                                         </div> 
                         )
                     })
-                }
+                } */}
 
                 
 
